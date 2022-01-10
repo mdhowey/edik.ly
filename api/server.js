512d652d870a9ceacb2ec2568ddc2dfb89499aea
devhowey <dev.howey@gmail.com>
2022-01-10 15:28:15 -0600

    server up and running

---
+++ api/server.js
@@ -0,0 +1,46 @@
+// import dependices
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// create Express app
+const app = express();
+
+// db setup
+// const db = require("./app/models");
+// db.sequelize.sync().then(() => {
+//   // @TODO delete console.log before deployment
+//   console.log("DB connected");
+// });
+// db.sequelize.sync({force:true}).then(() => {
+//   console.log("Drop and re-sync happened successfully in DB object.")
+// });
+
+// configure cors optinos
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+// add cors
+app.use(cors(corsOptions));
+
+// parse requests (json)
+app.use(bodyParser.json());
+
+// @TODO what is this?
+// content-type --> application/x-www-form-urlencoded ????
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// require routes for app
+// require("./app/routes/tutorial.routes")(app);
+
+// PING route
+app.get("/ping", (req, res) => {
+  res.json({ message: "This is the ping route. We are being pinged." });
+});
+
+// set port and listen
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server up and running at: http://localhost:${PORT}`);
+});
